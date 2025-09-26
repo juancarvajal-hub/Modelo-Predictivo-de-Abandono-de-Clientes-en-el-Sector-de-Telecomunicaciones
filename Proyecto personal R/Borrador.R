8adb3e9ad319aeb7fc2c8d0cee1e35de93d7d52e
juancarvajal-hub <81785581+juancarvajal-hub@users.noreply.github.com>
2025-09-26 00:50:56 -0500

    Add files via upload
    Actualizacion tipo de datos de variables

--- Proyecto personal R/Borrador.R
+++ Proyecto personal R/Borrador.R
@@ -8,7 +8,9 @@ require(xtable)
 rm(list = ls())
 gc()
 
+################################################################################
 ### ---------------------- Lectura de la base de datos ----------------------###
+################################################################################
 
 datos <- read.csv("WA_Fn-UseC_-Telco-Customer-Churn.csv", header = TRUE, sep = ",")
 cat("la base de datos cuenta con",dim(datos),"filas y columnas respectivamente")
@@ -35,7 +37,10 @@ ggplot(resumen1, aes(x = reorder(variables, porcentaje_completo),
 
 cat("Base de datos bastante completa se evidencia poca presencia de valores faltantes" )
 
+
+################################################################################
 ## ---------------- Análisis exploratorio de la información ------------------##
+################################################################################
 
 # Sabemos que tenemos 21 variables en nuestra base de datos , lo que sigue es analizar si
 # hay variables que presenten problemas como registros mal tomados o revisemos como el R esta 
@@ -68,34 +73,24 @@ datos %>%
 datos$SeniorCitizen <- as.character(datos$SeniorCitizen)
 datos$gender <- recode(datos$gender,"Female"="1",
                        "Male"="0")
-# Partner
-# datos$Partner <- recode(datos$Partner,"Yes"="1",
-#                         "No"="0")
 # MultipleLines
 datos$MultipleLines <- recode(datos$MultipleLines,"No phone service"="No") 
-
 # OnlineSecurity
 datos$OnlineSecurity <- recode(datos$OnlineSecurity,"No internet service"="No")
-
 # OnlineBackup
 datos$OnlineBackup <- recode(datos$OnlineBackup,"No internet service"="No")
-
 # DeviceProtection
 datos$DeviceProtection <- recode(datos$DeviceProtection,"No internet service"="No")
-
 # TechSupport
 datos$TechSupport <- recode(datos$TechSupport,"No internet service"="No")
-
 # StreamingTV
 datos$StreamingTV <- recode(datos$StreamingTV,"No internet service"="No")
-
 # StreamingMovies
 datos$StreamingMovies <- recode(datos$StreamingMovies,"No internet service"="No")
 
 
 # Convertir en factor todas las variables de tipo character 
 datos %<>% mutate(across(where(is.character),as.factor))
-
 datos %<>% mutate(across(where(~is.factor(.) && all(levels(.) %in% c("Yes","No"))),
                         ~ ifelse(.=="Yes","1","0")))
 datos %<>% mutate(across(where(is.character),as.factor))
@@ -105,5 +100,14 @@ datos %>% str()
 
 data.frame(variables = c(names(datos)),
            tipo = sapply(datos,class))
+
+
+################################-------------###################################
+###---------------------- Primeras gráficas descriptivas --------------------### 
+################################-------------###################################
+
+
+
+
 
                  
